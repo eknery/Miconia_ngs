21fd3aedb379098d58d0069de228524538903d6d
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2025-03-17 10:38:23 -0300

    update

---
+++ scripts/concatenating_sequences.R
@@ -0,0 +1,39 @@
+### library
+library("seqinr")
+### all species names
+spp_names = read.table("spp_names.txt", h=F)
+spp_names = spp_names$V1
+### all species names
+n_spp = read.table("n_spp.txt", h=F)
+n_spp = n_spp$V1
+### list file names
+loci_names = list.files(path = "2_completed_aligned_sequences", pattern = ".FNA")
+### loci and number of sequences
+loci_nspp = as.data.frame(cbind(loci_names,  as.integer(n_spp)))
+### pick the first aligment
+concatenation = read.fasta(paste0("2_completed_aligned_sequences/", loci_names[1]))
+### loop
+for(i in 2:length(loci_names) ){
+  ### pick other locus
+  other_locus = read.fasta(paste0("2_completed_aligned_sequences/", 
+                                  loci_names[i]))
+  ### retrieve number of species with data
+  nspp_data = as.integer(loci_nspp$V2[loci_nspp$loci_names == loci_names[i]])
+  ### check if aligment has enough data
+  if(nspp_data >= 74){
+    ### add sequences from other locus
+    for(sp_name in spp_names){
+      concatenation[[sp_name]] = c(concatenation[[sp_name]], other_locus[[sp_name]])
+    }
+  }
+}
+
+### export
+dir_out = "3_concatenated_sequences/"
+write.fasta(
+  sequences = concatenation, 
+  as.string = F, 
+  names = spp_names,
+  file.out = paste0(dir_out, "all_loci.fasta"),
+  nbchar = 100
+)
--- scripts/preparing_to_concatenate.R
+++ scripts/preparing_to_concatenate.R
@@ -1,39 +1,47 @@
+### library
 library("seqinr")
-
 ### all species names
-all_names = read.table("all_names.txt", h=F)
-all_names = all_names$V1
-
+spp_names = read.table("spp_names.txt", h=F)
+spp_names = spp_names$V1
 ### list file names
-locus_names = list.files(path = "1_aligned_sequences", pattern = ".FNA")
-
+loci_names = list.files(path = "1_aligned_sequences", pattern = ".FNA")
 ### number of species in each aligment
 n_spp = c()
-
-### pick one fasta aligment
-one_locus = read.fasta(paste0("1_aligned_sequences/", locus_names[1]))
-### update n_spp
-n_spp = c(n_spp, length(one_locus) )
-### including missing species
-n_patterns = length(one_locus[[1]])
-for(name in all_names){
-  boll = name %in% names(one_locus)
-  if(boll == FALSE){
-    one_locus[[name]] = as.integer(rep("-", n_patterns))
-  }
-}
-### ordering sepcies
-one_locus = one_locus[all_names]
-
-### exporting sequences 
-dir_out = "3_final_sequences/"
-for(i in 1:length(my_fasta_list)){
-  fasta_name = paste0(names(my_fasta_list)[i], ".fasta")
-  write.phyDat(x = my_fasta_list[[i]], 
-               file = paste0(dir_out,fasta_name), 
-               format = "fasta", 
-               colsep = "", 
-               nbcol =100
+### loop 
+for(i in 1:length(loci_names)){
+  tryCatch(
+    {
+      ### pick one fasta aligment
+      one_locus = read.fasta(paste0("1_aligned_sequences/", loci_names[i]))  
+      ### update n_spp
+      n_spp = c(n_spp, length(one_locus) )
+      ### including missing species
+      n_patterns = length(one_locus[[1]])
+      for(name in spp_names){
+        boll = name %in% names(one_locus)
+        if(boll == FALSE){
+          one_locus[[name]] = rep("-", n_patterns)
+        }
+      }
+      ### ordering species
+      one_locus = one_locus[spp_names]
+      ### output directory 
+      dir_out = "2_completed_aligned_sequences/"
+      ### exporting sequences 
+      write.fasta(
+        sequences = one_locus, 
+        as.string = F, 
+        names = spp_names,
+        file.out = paste0(dir_out, loci_names[i]),
+        nbchar = 100
+      )
+    },
+    error = function(e) {
+      n_spp = c(n_spp, 0)
+      cat("Skipping: ", loci_names[i])
+      return(NULL)  # Return NULL to indicate failure
+    }
   )
 }
 
+write.table(n_spp,"n_spp.txt", row.names = F)
